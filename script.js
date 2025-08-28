21c20698c19d9fc27047c4770f23b114ac47e5ea
b <66017805+bryanseah234@users.noreply.github.com>
2025-08-28 12:34:06 +0000

    new function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function me() {
     console.log('hello@hong-yi.me')
 }
+
+function randompfp() {
+    let img = document.getElementById("image")
+    img.src = "https://avatars.githubusercontent.com/u/" + Math.floor(Math.random() * 100000000);
+}
